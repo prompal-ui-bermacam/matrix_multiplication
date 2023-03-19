bf336ee18ef7f38623db286bcd0b5bcda0b4686d
Prajna Prasetya <prajnapras19@gmail.com>
2023-03-19 22:59:49 +0700

    test env b c

--- test_mpi-2.sh
+++ test_mpi-2.sh
@@ -12,6 +12,8 @@ NPS=(
     2
     4
     8
+    16
+    32
 )
 MATRIX_SIZES=(
     128
@@ -23,22 +25,22 @@ MATRIX_SIZES=(
 
 for SZ in ${MATRIX_SIZES[@]}
 do 
-    echo "* * * * * * * ${SZ}x${SZ} Matrix" >> test-matrix.txt
+    echo "* * * * * * * ${SZ}x${SZ} Matrix" | tee test-matrix.txt
     for PROC in ${NPS[@]}
     do
         cal_t=$((time mpirun -np ${PROC} bin/mpi data/mat_${SZ}x${SZ}.txt data/mat_${SZ}x${SZ}b.txt)  2>&1 > /dev/null | grep real | awk '{print $2}')
-        echo "with mpi(${PROC})     $cal_t" >> test-matrix.txt
-        echo "" >> test-matrix.txt
+        echo "with mpi(${PROC})     $cal_t"  | tee test-matrix.txt
+        echo ""  | tee test-matrix.txt
     done
 done
 
 for SZ in ${MATRIX_SIZES[@]}
 do 
-    echo "* * * * * * * ${SZ}x1 Matrix" >> test-vector.txt
+    echo "* * * * * * * ${SZ}x1 Matrix" | tee test-vector.txt
     for PROC in ${NPS[@]}
     do
         cal_t=$((time mpirun -np ${PROC} bin/mpi data/mat_${SZ}x${SZ}.txt data/mat_${SZ}x1.txt)  2>&1 > /dev/null | grep real | awk '{print $2}')
-        echo "with mpi(${PROC})     $cal_t" >> test-vector.txt
-        echo "" >> test-vector.txt
+        echo "with mpi(${PROC})     $cal_t" | tee test-vector.txt
+        echo | tee test-vector.txt
     done
 done
